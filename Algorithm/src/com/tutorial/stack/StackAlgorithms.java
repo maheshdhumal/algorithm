3bb97b801bf4f568808a2b0b8a07a35ea6960e6c
maheshdhumal <mdismyid87@gmail.com>
2017-02-26 21:53:20 +0530

    stack algos

--- Algorithm/src/com/tutorial/stack/StackAlgorithms.java
+++ Algorithm/src/com/tutorial/stack/StackAlgorithms.java
@@ -163,20 +163,104 @@ public class StackAlgorithms {
 		}
 	}
 	
-	
-	
-	
-	public static void main(String[] args)
+	private void printNextGreaterElement(int[] array)
 	{
-		StackAlgorithms stackAlgorithms=new StackAlgorithms();
-		//stackAlgorithms.eveluatePostFix("( A + B / c * ( D + E ) - F)");
-		/*stackAlgorithms.evelauatePrefix("+ + 2 * 3 2 / 10 2");
-		System.out.println(""+s);
-		stackAlgorithms.eveluatePostFix("2 3 2 * * 10 2 / +");*/
-		//String s= stackAlgorithms.infixToPrefix("2 + 3 * 2 + 10 / 2");
-		//System.out.println(""+s);
-		String s=stackAlgorithms.infixToPostfix("( A + B / c * ( D + E ) - F )");
-		System.out.println(""+s);
+		Stack<Integer> stack=new Stack<Integer>();
+		for(int i=0;i<array.length;i++)
+		{
+			for(int k=i+1;k<array.length;k++)
+			{
+				if(array[i] < array[k])
+				{
+					stack.push(array[k]);
+					break;
+				}
+			}
+			if(stack.isEmpty())
+			{
+				System.out.println(""+-1);
+			}
+			else
+			{
+				System.out.println(""+stack.pop());
+			}
+		}
+	}
+	
+	private void printNextGreaterElementUsingStack(int[] array) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(array[0]);
+		int k;
+		for (int i = 1; i < array.length; i++) {
+			int next = array[i];
+			if (!stack.isEmpty()) {
+				k = stack.pop();
+					while (k < next) {
+						System.out.println(+k+"  "+ next);
+						if (stack.isEmpty()) {
+							break;
+						}
+						k=stack.pop();
+					}
+					if(k>=next)
+					{
+					 stack.push(k);
+					}
+				}
+			stack.push(next);
+		}
+		while(!stack.isEmpty())
+		{
+			int y=stack.pop();
+			System.out.println(+y+"  "+-1);
+			
+		}
+	}
+	
+	
+	private Stack<Integer> reverseStack(Stack<Integer> stack)
+	{
+		if(stack.isEmpty())
+		{
+			return stack;
+		}
+		int element=stack.pop();
+		reverseStack(stack);
+		sort(stack,element);
+		return stack;
+	}
+	
+	
+	private void pushData(Stack<Integer> stack, int element) {
+		if(stack.isEmpty())
+		{
+			 stack.push(element);
+			 return;
+		}
+		int temp=stack.pop();
+		pushData(stack,element);
+		stack.push(temp);
+	}
+	
+	private void sort(Stack<Integer> stack,int element)
+	{
+		if(stack.isEmpty())
+		{
+			stack.push(element);
+			return;
+		}
+		int temp=stack.pop();
+		if(temp < element)
+		{
+			sort(stack,element);
+			stack.push(temp);
+		}
+		else
+		{
+			stack.push(temp);
+			stack.push(element);
+		}
+		
 	}
 
 	private String infixToPrefix(String exp) 
@@ -190,4 +274,51 @@ public class StackAlgorithms {
 		}
 		return infixToPostfix(buffer.toString());
 	}
+	
+	private void calculateSpan(int[] prices)
+	{
+		int[] span =new int[prices.length];
+		span[0]=1;
+		Stack<Integer> stack=new Stack<Integer>();
+		stack.push(0);
+		for(int i=1;i < prices.length;i++)
+		{
+			while(!stack.isEmpty() && prices[stack.peek()] <= prices[i])
+			{
+				stack.pop();
+			}
+			span[i]=stack.isEmpty() ? i+1:(i-stack.peek());
+			stack.push(i);
+		}
+		for(int i=0;i<span.length;i++)
+		{
+			System.out.println(""+span[i]);
+		}
+	}
+	public static void main(String[] args)
+	{
+		StackAlgorithms stackAlgorithms=new StackAlgorithms();
+		//stackAlgorithms.eveluatePostFix("( A + B / c * ( D + E ) - F)");
+		/*stackAlgorithms.evelauatePrefix("+ + 2 * 3 2 / 10 2");
+		System.out.println(""+s);
+		stackAlgorithms.eveluatePostFix("2 3 2 * * 10 2 / +");*/
+		//String s= stackAlgorithms.infixToPrefix("2 + 3 * 2 + 10 / 2");
+		//System.out.println(""+s);
+		//String s=stackAlgorithms.infixToPostfix("( A + B / c * ( D + E ) - F )");
+		//System.out.println(""+s);
+		int[] array={100, 80, 60, 70, 60, 75, 85};
+		//stackAlgorithms.calculateSpan(array);
+		Stack<Integer> stack=new Stack<Integer>();
+		for(int i=0;i<array.length;i++)
+		{
+			stack.push(array[i]);
+		}
+		stack=stackAlgorithms.reverseStack(stack);
+		while(!stack.isEmpty())
+		{
+			System.out.println(""+stack.pop());
+		}
+	}
+
+	
 }
